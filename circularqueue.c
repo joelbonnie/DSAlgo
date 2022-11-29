18071b7070380c6ff4890aae308cbe5123e1b49e
Naveena Pius <naveenapius@protonmail.com>
2022-11-30 01:40:24 +0530

    added comments. fixed display

--- circularqueue.c
+++ circularqueue.c
@@ -6,7 +6,9 @@ int cq[SIZE];
 int front=-1, rear=-1;
 
 
-int insert(int e){
+int enqueue(int e){
+    //enequeus element
+    //displays message if queue is full
     if((rear+1)%SIZE==front){
         printf("\nQueue is full. No more elements can be inserted.");
         return -1;
@@ -19,7 +21,9 @@ int insert(int e){
     return 0;
 }
 
-int del(){
+int dequeue(){
+    //dequeues element
+    //displays message if queue is empty
     if(front == -1){
         printf("\nQueue is empty. Nothing to delete");
         return -1;
@@ -35,8 +39,11 @@ int del(){
 }
 
 void display(){
+    //displays current queue
+    //displays message if queue is empty
     if(front == -1){
-        printf("\nQueue is empty. Nothing to display");
+        printf("\nQueue is empty. Nothing to display\n");
+        return;
     }
     int i;
     for(i=front; i != rear; i=(i+1)%SIZE){
@@ -48,15 +55,15 @@ void display(){
 
 void main(){
 	int opt, e;
-	printf("\nEnter an option(1-insert, 2-delete, 3-display, 4-quit): ");
+	printf("\nEnter an option(1-enqueue, 2-dequeue, 3-display, 4-quit): ");
 	scanf("%d: ",&opt);
 	while(opt!=4){
 			if(opt==1){
 				printf("\nEnter element for insertion: ");
 				scanf("%d: ",&e);
-				insert(e);
+				enqueue(e);
 			}else if(opt==2){
-				e = del();
+				e = dequeue();
 			}else if(opt==3){
 				display();
 			}else{
